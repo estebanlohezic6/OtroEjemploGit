8bc5b9c2c4959e65b1c1654f7c9a09a651e42a2b
estebanlohezic6 <estebanlohezi@gmail.com>
2018-10-02 17:11:30 -0300

    Rama de Desarrollo

--- src/otroejemplogit/OtroEjemploGit.java
+++ src/otroejemplogit/OtroEjemploGit.java
@@ -7,6 +7,7 @@ public class OtroEjemploGit {
         System.out.println("El primer cambio a este proyecto.");
         System.out.println("Version 2.0");
         System.out.println("El segundo cambio a este proyecto.");
+        System.out.println("En la rama de Desarrollo.");
     }
     
 }
