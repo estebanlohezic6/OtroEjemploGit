e89886ff44f138d89dc2627069dc838dce041b4b
estebanlohezic6 <estebanlohezi@gmail.com>
2018-10-02 17:00:10 -0300

    Version 1.0

--- src/otroejemplogit/OtroEjemploGit.java
+++ src/otroejemplogit/OtroEjemploGit.java
@@ -3,6 +3,7 @@ package otroejemplogit;
 public class OtroEjemploGit {
 
     public static void main(String[] args) {
+        System.out.println("Version 1.0");
         System.out.println("El primer cambio a este proyecto.");
     }
     
