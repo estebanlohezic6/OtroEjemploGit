3c60f1111355604b317613999a762aba2d5f2e74
estebanlohezic6 <estebanlohezi@gmail.com>
2018-10-02 17:04:42 -0300

    Version 2.0

--- src/otroejemplogit/OtroEjemploGit.java
+++ src/otroejemplogit/OtroEjemploGit.java
@@ -5,6 +5,8 @@ public class OtroEjemploGit {
     public static void main(String[] args) {
         System.out.println("Version 1.0");
         System.out.println("El primer cambio a este proyecto.");
+        System.out.println("Version 2.0");
+        System.out.println("El segundo cambio a este proyecto.");
     }
     
 }
