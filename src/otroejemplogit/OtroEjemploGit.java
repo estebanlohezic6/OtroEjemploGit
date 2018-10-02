20b1452c4a92365b892b026e1a694e381fd3fb42
estebanlohezic6 <estebanlohezi@gmail.com>
2018-10-02 16:51:55 -0300

    Primer Cambio

--- src/otroejemplogit/OtroEjemploGit.java
+++ src/otroejemplogit/OtroEjemploGit.java
@@ -3,7 +3,7 @@ package otroejemplogit;
 public class OtroEjemploGit {
 
     public static void main(String[] args) {
-        
+        System.out.println("El primer cambio a este proyecto.");
     }
     
 }
